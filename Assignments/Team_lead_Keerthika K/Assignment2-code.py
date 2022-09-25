432ffc5fcd2314ad51adbed7ab06e05b73bea68a
keerthika k <78524216+keerthi076@users.noreply.github.com>
2022-09-25 19:16:17 +0530

    Add files via upload
    Signed-off-by: keerthika k <78524216+keerthi076@users.noreply.github.com>

---
+++ Assignments/Team_lead_Keerthika K/Assignment2-code.py
@@ -0,0 +1,22 @@
+import random
+
+
+temp=random.randint(1,100)
+humidity=random.randint(1,100)
+
+print("temperature:",temp)
+if temp > 25 and temp < 50:
+    print("temperature is normal")
+elif temp < 25:
+    print("temperature is low")
+else:
+    print("temperature is  high")
+
+print("Humidity:",humidity)
+if humidity> 31 and humidity < 60:
+    print("humidity is normal")
+elif humidity < 31:
+    print("humidity is low")
+else:
+    print("humidity is  high")
+    
