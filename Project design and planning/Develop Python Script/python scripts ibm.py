b893274755d80104c7b3f1882cb10a98f59c848d
keerthika k <78524216+keerthi076@users.noreply.github.com>
2022-11-18 22:44:48 +0530

    Add files via upload
    Signed-off-by: keerthika k <78524216+keerthi076@users.noreply.github.com>

---
+++ Project design and planning/Develop Python Script/python scripts ibm.py
@@ -0,0 +1,61 @@
+import random
+import time
+import sys
+import ibmiotf.application
+import ibmiotf.device
+
+
+# Provide your IBM Watson Device Credentials
+
+organization = "kuhkhy"
+devicType = "python"
+deviceId = "pythonscript"
+authMethod= "use-token-auth"
+authToken= "sAKT&OrB1NguaZ8Z@s"
+
+
+def myCommandCallback(cmd):
+    print("Command received: %s" % cmd.data['command'])
+    status=cmd.data['command']
+    if status == 'lighton':
+        print("LIGHT ON")
+    elif status == 'lightoff':
+        print("LIGHT OFF")
+    else:
+        print ("please send proper command")
+
+
+try:
+    deviceOptions = {"org": organization, "type": deviceType, "id": deviceId, "auth-method": authMethod,
+                     "auth-token": authToken}
+    deviceCli = ibmiotf.device.Client(deviceOptions)
+# ..............................................
+
+except Exception as e:
+    print("Caught exception connecting device: %s" % str(e))
+    sys.exit()
+
+deviceCli.connect()
+
+while True:
+    w= random.randint(0,100)
+    l = random.randint(0,100)
+   
+    # Send Temperature & Humidity to IBM Watson
+    data = {'weight': w,'level':l}
+
+
+    # print data
+    def myOnPublishCallback():
+        print("Published data",data, "to IBM Watson")
+
+
+    success = deviceCli.publishEvent("event", "json", data, 0, myOnPublishCallback)
+    if not success:
+        print("Not connected to IoTF")
+    time.sleep(5)
+
+    deviceCli.commandCallback = myCommandCallback
+
+# Disconnect the device and application from the cloud
+devicecli.disconnect()
